7fc062ecda582d627d0bc9b91e4534f27b136fae
chinaza-max <mosesogbonna68@gmail.com>
2024-02-20 14:16:12 +0100

    debugging

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -2714,151 +2714,229 @@ async  sendEmailVerificationCode(emailAddress, userId ,password) {
     return password;
   }
  
-    
- async rematchUser(){
+  async rematchUser(){
   
-  try {
-    const usersWithProfiles =await  this.UserModel.findAll({
-      attributes: ['id', 'tags'],
-      include: [{
-        model: this.UserAnswerModel,
-        as: 'UserAnswers',
-        attributes: ['answer', 'partnerPersonaltyQId'],
+    try {
+      const usersWithProfiles =await  this.UserModel.findAll({
+        attributes: ['id', 'tags'],
+        include: [{
+          model: this.UserAnswerModel,
+          as: 'UserAnswers',
+          attributes: ['answer', 'partnerPersonaltyQId'],
+          where: {
+            isDeleted:false
+          },
+        }],
         where: {
+          isTelValid:true,
+          isEmailValid:true,
           isDeleted:false
         },
-      }],
-      where: {
-        isTelValid:true,
-        isEmailValid:true,
-        isDeleted:false
-      },
-    });
-    
-    let UserInfo=[]
-    for (let index = 0; index < usersWithProfiles.length; index++) {
-      const userArray = usersWithProfiles[index];
-      const tags=JSON.parse(userArray.dataValues.tags)
+      });
       
-
-      let answerAndquestionIdArray=[]
-      for (let index2 = 0; index2 < userArray.UserAnswers.length; index2++) {
-        const userAnswerArray = userArray.UserAnswers[index2];
-        const answer=userAnswerArray.dataValues.answer
-        const questionId=userAnswerArray.dataValues.partnerPersonaltyQId
-        const answerAndquestionId=answer+'_'+questionId
-        answerAndquestionIdArray.push(answerAndquestionId)
-      }
-      const combinedArray = [...tags,...answerAndquestionIdArray];
-
-      UserInfo.push({userId:userArray.dataValues.id,userData:combinedArray,preferedGender:userArray.dataValues.preferedGender,gender:userArray.dataValues.gender})
-
-    }
-
-
-    function calculateMatchingPercentage(set1, set2) {
-      const intersection = set1.filter(value => set2.includes(value));
-      const matchingPercentage = Math.round((intersection.length / set1.length) * 100);
-      return matchingPercentage;
-    }
-    
-    function findMatchingUsers(data, threshold) {
-      const matchingUsers = [];
-    
-      for (let i = 0; i < data.length - 1; i++) {
-        for (let j = i + 1; j < data.length; j++) {
-          const user1 = data[i];
-          const user2 = data[j];
-
-          if(user1.preferedGender==user2.gender&&user2.gender==user1.preferedGender){
-            const matchingPercentage = calculateMatchingPercentage(user1.userData, user2.userData);
-    
-            if (matchingPercentage >= threshold) {
-              const matchingData = user1.userData.filter(value => user2.userData.includes(value));
-              matchingUsers.push({
-                userId1: user1.userId,
-                userId2: user2.userId,
-                matchingPercentage,
-                matchingData,
-              });
-            }
-          }else{
-            continue
-          }
-          
-        }
-      }
-
-      function cleanUpMatchingData(matchingUsers) {
-        return matchingUsers.map(match => ({
-          userId1: match.userId1,
-          userId2: match.userId2,
-          matchingPercentage: match.matchingPercentage,
-          matchingData: match.matchingData.map(value => value.split('_')[0]),
-        }));
-      }
-      return cleanUpMatchingData(matchingUsers);
-    }
-    
-    const threshold = 50; 
-    const result = findMatchingUsers(UserInfo, threshold);
-
-    try {  
-
-      for (let index = 0; index < result.length; index++) {
-        const element = result[index];
-
+      let UserInfo=[]
+      for (let index = 0; index < usersWithProfiles.length; index++) {
+        const userArray = usersWithProfiles[index];
+        const tags=JSON.parse(userArray.dataValues.tags)
         
-        const existingMatch1 = await this.UserMatchModel.findOne({
-          where: {   
-            userId:element.userId1,
-            userId2:element.userId2,
-            isDeleted:false },
-        });
-
-        const existingMatch2 = await this.UserMatchModel.findOne({
-          where: {   
-            userId:element.userId2,
-            userId2:element.userId1,
-            isDeleted:false },
-        });
-
-
-        if (existingMatch1) {
-
-          await existingMatch1.update({
-            matchInformation:JSON.stringify(element.matchingData),
-            matchPercentage:element.matchingPercentage+'%'
-          });
-        } else if(existingMatch2) {
-          await existingMatch2.update({
-            matchInformation:JSON.stringify(element.matchingData),
-            matchPercentage:element.matchingPercentage+'%'
-          });
+         
+        let answerAndquestionIdArray=[]
+        for (let index2 = 0; index2 < userArray.UserAnswers.length; index2++) {
+          const userAnswerArray = userArray.UserAnswers[index2];
+          const answer=userAnswerArray.dataValues.answer
+          const questionId=userAnswerArray.dataValues.partnerPersonaltyQId
+          const answerAndquestionId=answer+'_'+questionId
+          answerAndquestionIdArray.push(answerAndquestionId)
         }
-        else{
-          await this.UserMatchModel.create({
-            userId:element.userId1,
-            userId2:element.userId2,
-            matchInformation:JSON.stringify(element.matchingData),
-            matchPercentage:element.matchingPercentage
-        });
-        }
-
+        const combinedArray = [...tags,...answerAndquestionIdArray];
+  
+        UserInfo.push({userId:userArray.dataValues.id,userData:combinedArray,preferedGender:userArray.dataValues.preferedGender,gender:userArray.dataValues.gender})
+  
       }
-
-  } catch (error) {
-    console.log(error);
-    throw new SystemError(error.name,error.parent)
+  
+  
+      function calculateMatchingPercentage(set1, set2) {
+        const intersection = set1.filter(value => set2.includes(value));
+        const matchingPercentage = Math.round((intersection.length / set1.length) * 100);
+        return matchingPercentage;
+      }
+      
+      function findMatchingUsers(data, threshold) {
+        const matchingUsers = [];
+  
+        for (let i = 0; i < data.length - 1; i++) {
+          for (let j = i + 1; j < data.length; j++) {
+            const user1 = data[i];
+            const user2 = data[j];
+     
+  
+            if(user1.preferedGender==user2.gender&&user2.gender==user1.preferedGender){
+  
+              const matchingPercentage = calculateMatchingPercentage(user1.userData, user2.userData);
+    
+              if (matchingPercentage >= threshold) {
+                const matchingData = user1.userData.filter(value => user2.userData.includes(value));
+                matchingUsers.push({
+                  userId1: user1.userId,
+                  userId2: user2.userId,
+                  matchingPercentage,
+                  matchingData,
+                });
+              }
+            }else{
+              continue
+            }
+            
+          }
+        }
+  
+        function cleanUpMatchingData(matchingUsers) {
+          return matchingUsers.map(match => ({
+            userId1: match.userId1,
+            userId2: match.userId2,
+            matchingPercentage: match.matchingPercentage,
+            matchingData: match.matchingData.map(value => value.split('_')[0]),
+          }));
+        }
+        return cleanUpMatchingData(matchingUsers);
+      }
+      
+      const threshold = 50; 
+      const result = findMatchingUsers(UserInfo, threshold);
+  
+      try {  
+  
+        for (let index = 0; index < result.length; index++) {
+          const element = result[index];
+          element.matchingData=await this.getCommonBioDetail(element.userId1,element.userId2,)
+          
+          const existingMatch1 = await this.UserMatchModel.findOne({
+            where: {   
+              userId:element.userId1,
+              userId2:element.userId2,
+              isDeleted:false },
+          });
+  
+          const existingMatch2 = await this.UserMatchModel.findOne({
+            where: {   
+              userId:element.userId2,
+              userId2:element.userId1,
+              isDeleted:false },
+          });
+  
+  
+          if (existingMatch1) {
+  
+            console.log("updating updating")
+  
+            console.log(element)
+  
+            console.log("updating updating")
+  
+  
+            await existingMatch1.update({
+              matchInformation:JSON.stringify(element.matchingData),
+              matchPercentage:element.matchingPercentage
+            });
+          } else if(existingMatch2) {
+  
+  
+            
+            console.log("updating updating")
+  
+            console.log(element)
+  
+            console.log("updating updating")
+  
+  
+  
+            await existingMatch2.update({
+              matchInformation:JSON.stringify(element.matchingData),
+              matchPercentage:element.matchingPercentage
+            });
+          }
+          else{
+            await this.UserMatchModel.create({
+              userId:element.userId1,
+              userId2:element.userId2,
+              matchInformation:JSON.stringify(element.matchingData),
+              matchPercentage:element.matchingPercentage
+          });
+          }
+  
+        }
+  
+    } catch (error) {
+      console.log(error);
+      throw new SystemError(error.name,error.parent)
+    }
+  
+  
+    } catch (error) {
+      console.log(error)
+      throw new SystemError(error.name,error.parent)
+    }
+    
   }
+  
+  
+async getCommonBioDetail(userId1,userId2){
+  
+  try {
+   
+    let user1Details=await this.UserModel.findByPk(userId1)
+    let user2Details=await this.UserModel.findByPk(userId2)
+    let result=[]
 
+    if(user1Details.relationshipGoal==user2Details.user2Details){
+        result.push('Relationship Goal')
+    }
+    if(user1Details.maritalStatus==user2Details.maritalStatus){
+      result.push('marital Status')
+    }
+    if(user1Details.numberOfChildren==user2Details.numberOfChildren){
+      result.push('Number of Children')
+    }
+
+    if(user1Details.language==user2Details.language){
+      result.push('Language')
+    }
+    if(user1Details.ethnicity==user2Details.ethnicity){
+      result.push('Ethnicity')
+    }
+    if(user1Details.religion==user2Details.religion){
+      result.push('Religion')
+    }
+
+    if(user1Details.education==user2Details.education){
+      result.push('Education')
+    }
+
+    if(user1Details.courseOfStudy==user2Details.courseOfStudy){
+      result.push('Course of Study')
+    }
+
+    if(user1Details.occupation==user2Details.occupation){
+      result.push('Profession')
+    }
+
+    if(user1Details.recreationalActivity==user2Details.recreationalActivity){
+      result.push('Hobby')
+    }
+
+    if(user1Details.haveChildren==user2Details.haveChildren){
+      result.push('Single Parent')
+    }
+
+    return result
 
   } catch (error) {
     console.log(error)
     throw new SystemError(error.name,error.parent)
   }
   
- }
+}
+
 
 
 }
