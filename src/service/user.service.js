ab8ec573efdb8c9b42be18cc2aeb3909aa5d5bfb
chinaza-max <mosesogbonna68@gmail.com>
2024-03-03 19:37:24 +0100

    add new attribute numberOfBusinessSpot

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -879,7 +879,7 @@ class UserService {
         dateDetails.update({
           reservationStatus:'decline'
         })
-      }
+      }  
     }
     else if(type=='acceptReservationStatus'){
       if (dateDetails){
