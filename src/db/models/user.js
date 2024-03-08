6ad982b05ff8b45c8a143646a1aa75da32e5ab93
chinaza-max <mosesogbonna68@gmail.com>
2024-03-08 21:07:15 +0100

    change user model

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -174,7 +174,7 @@ export function init(connection) {
         defaultValue:false
       },
       notificationAllowed: {
-        type: DataTypes.STRING,
+        type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue:true
       },
