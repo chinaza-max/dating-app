c205f11f5254624a4cec8cd32da4d6523ceac136
chinaza-max <mosesogbonna68@gmail.com>
2024-03-17 15:49:05 +0100

    remove big file

--- src/service/Push.notification.service.js
+++ src/service/Push.notification.service.js
@@ -29,7 +29,7 @@ class PushNotificationService {
         "client_x509_cert_url": serverConfig.FIREBASE_CLIENT_X509_CERT_URL,
         "universe_domain": serverConfig.FIREBASE_UNIVERSAL_DOMAIN
       }),
-      projectId:'choice-mi'
+      project_id:'choice-mi'
     });
 
 
