544bf28117ccdd548f843dab74172d2e2df935a3
chinaza-max <mosesogbonna68@gmail.com>
2024-03-17 15:59:18 +0100

    remove big file

--- src/service/Push.notification.service.js
+++ src/service/Push.notification.service.js
@@ -14,7 +14,17 @@ class PushNotificationService {
 
   async init() {
    
-      
+      console.log({"type": serverConfig.FIREBASE_TYPE,
+      "project_id": serverConfig.FIREBASE_PROJECT_ID,
+      "private_key_id": serverConfig.FIREBASE_PRIVATE_KEY_ID,
+      "private_key": serverConfig.FIREBASE_PRIVATE_KEY,
+      "client_email": serverConfig.FIREBASE_CLIENT_EMAIL,
+      "client_id":serverConfig.FIREBASE_CLIENT_ID,
+      "auth_uri": serverConfig.FIREBASE_AUTH_URI,
+      "token_uri": serverConfig.FIREBASE_TOKEN_URI,
+      "auth_provider_x509_cert_url": serverConfig.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+      "client_x509_cert_url": serverConfig.FIREBASE_CLIENT_X509_CERT_URL,
+      "universe_domain": serverConfig.FIREBASE_UNIVERSAL_DOMAIN})
     admin.initializeApp({
       credential: admin.credential.cert({
         "type": serverConfig.FIREBASE_TYPE,
