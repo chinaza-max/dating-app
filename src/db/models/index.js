4a35c83135b3b2e2db6a1e2440ddfdccecc468b5
chinaza-max <mosesogbonna68@gmail.com>
2024-03-21 14:41:09 +0100

    change marketing data

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -225,7 +225,7 @@ function associate() {
 async function authenticateConnection(connection) {
   try {
     await connection.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log('Connection to database has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
