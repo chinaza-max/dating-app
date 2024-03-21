262799613357f838ddf4209fa5c21ca3f886695f
chinaza-max <mosesogbonna68@gmail.com>
2024-03-21 14:52:02 +0100

    change marketing data

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -56,6 +56,7 @@ export function init(connection) {
       tel: {
         type: DataTypes.STRING,
         allowNull: false,
+        defaultValue:true
       },
       isTelValid: {
         type: DataTypes.BOOLEAN,
