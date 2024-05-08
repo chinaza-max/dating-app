f65f0c9ea6691ce64ff175d35469302188d1f12e
chinaza-max <mosesogbonna68@gmail.com>
2024-05-08 20:15:41 +0100

    businessPicture

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -2092,7 +2092,7 @@ class UserService {
               include: [
                 {
                   model: this.BusinessSpotsModel,
-                  attributes: ['id', 'name', 'address', 'city', 'openHours', 'closeHours', 'tel', 'locationCoordinate'],
+                  attributes: ['businessPicture','id', 'name', 'address', 'city', 'openHours', 'closeHours', 'tel', 'locationCoordinate'],
                   where: {
                     isDeleted: false,
                   },
