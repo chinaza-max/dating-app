d9c039f4fd809b5d51dd2b5f0ec949d91432e138
chinaza-max <mosesogbonna68@gmail.com>
2024-07-06 10:06:45 +0100

    cors issues

--- app.js
+++ app.js
@@ -174,9 +174,15 @@ class Server {
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.json());
         this.app.use(express.static(path.join(__dirname, 'public')));
-        this.app.use(cors({
+        /*this.app.use(cors({
           origin: '*'
-      }  ));
+      }  ));*/
+      this.app.use((req, res, next) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        next();
+      })
         this.app.use(routes); 
         this.app.use(systemMiddleware.errorHandler);
 
